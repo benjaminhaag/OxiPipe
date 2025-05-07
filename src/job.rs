29b321068c21936abf499f548df3aa55ed8cdd1c
Benjamin Haag <me@benjaminhaag.it>
2025-05-07 12:05:52 +0200

    save logs

--- src/job.rs
+++ src/job.rs
@@ -2,9 +2,11 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use log::{debug, info, error};
 use bollard::Docker;
-use bollard::container::{Config, CreateContainerOptions, StartContainerOptions, LogsOptions};
+use bollard::container::{Config, CreateContainerOptions, StartContainerOptions, LogsOptions, LogOutput};
 use bollard::models::HostConfig;
 use futures::StreamExt;
+use std::fs::File;
+use std::io::Write;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Job {
@@ -22,8 +24,49 @@ impl Job {
         info!("starting job {}", name);
 
         // Pull the image if not present
+        if let Err(e) = self.pull_image(docker).await {
+            error!("Error pulling image: {}", e);
+            return;
+        }
+
+        // Create container
+        
+        let mounts = match self.prepare_mounts(&name) {
+            Ok(m) => m,
+            Err(e) => {
+                error!("Failed to prepare mounts: {}", e);
+                return;
+            }
+        };
+
+
+        let container = match self.create_container(docker, &name, mounts).await {
+            Ok(id) => id,
+            Err(e) => {
+                error!("Error creating container: {}", e);
+                return;
+            }
+        };
+
+        // Start the container
+        info!("Starting container: {}", container);
+        if let Err(e) = docker.start_container(&container, None::<StartContainerOptions<String>>).await {
+            error!("Error starting container: {}", e);
+            return;
+        }
+
+        // Get logs
+        if let Err(e) = self.stream_logs(docker, &container, &name).await {
+            error!("Failed to stream logs: {}", e);
+            return;
+        }
+        info!("Job {} completed.", name);
+    }
+    
+    async fn pull_image(&self, docker: &Docker) -> Result<(), bollard::errors::Error> {
         debug!("downloading image {}", self.image);
-        let mut pull_stream = docker.create_image(
+
+        let mut stream = docker.create_image(
             Some(bollard::image::CreateImageOptions {
                 from_image: self.image.clone(),
                 ..Default::default()
@@ -32,28 +75,25 @@ impl Job {
             None
         );
 
-        while let Some(pull_result) = pull_stream.next().await {
-            match pull_result {
-                Ok(output) => {
-                    if let Some(status) = output.status {
-                        info!("Pull status: {}", status);
-                    }
-                },
-                Err(e) => {
-                    error!("Error pulling image: {}", e);
-                    return;
+        while let Some(result) = stream.next().await {
+            if let Ok(output) = result {
+                if let Some(status) = output.status {
+                    info!("Pull status: {}", status);
                 }
+            } else {
+                return Err(result.err().unwrap());
             }
         }
 
-        // Create container
-        let container_name = format!("oxipipe-{}", name);
-        
+        Ok(())
+    }
+
+    fn prepare_mounts(&self, job_name: &str) -> Result<Vec<String>, std::io::Error> {
         let mut mounts: Vec<String> = Vec::new();
 
         if let Some(artifacts_path) = &self.artifacts {
-            let mount_path = format!("/tmp/oxipipe/artifacts/{}/workspace", name);
-            fs::create_dir_all(&mount_path).unwrap();
+            let mount_path = format!("/tmp/oxipipe/artifacts/{}/workspace", job_name);
+            fs::create_dir_all(&mount_path)?;
             let mount = format!("{}:{}",mount_path, artifacts_path);
             mounts.push(mount);
         }
@@ -66,43 +106,48 @@ impl Job {
             }
         }
 
-        let create_result = docker
-            .create_container(
-                Some(CreateContainerOptions { 
-                    name: &container_name,
-                    platform: None,
-                }),
-                Config {
-                    image: Some(self.image.clone()),
-                    cmd: Some(self.command.clone()),
-                    env: self.environment.clone(),
-                    host_config: Some(HostConfig {
-                        auto_remove: Some(true),
-                        binds: Some(mounts),
-                        ..Default::default()
-                    }),
-                    ..Default::default()
-                },
-            )
-            .await;
+        Ok(mounts)
+    }
 
-        let container = match create_result {
-            Ok(info) => info.id,
-            Err(e) => {
-                error!("Error creating container: {}", e);
-                return;
-            }
+    async fn create_container(
+        &self,
+        docker: &Docker,
+        name: &str,
+        mounts: Vec<String>,
+    ) -> Result<String, bollard::errors::Error> {
+        let container_name = format!("oxipipe-{}", name);
+
+        let config = Config {
+            image: Some(self.image.clone()),
+            cmd: Some(self.command.clone()),
+            env: self.environment.clone(),
+            host_config: Some(HostConfig {
+                auto_remove: Some(true),
+                binds: Some(mounts),
+                ..Default::default()
+            }),
+            ..Default::default()
         };
 
-        // Start the container
-        info!("Starting container: {}", container);
-        if let Err(e) = docker.start_container(&container, None::<StartContainerOptions<String>>).await {  error!("Error starting container: {}", e);
-            return;
-        }
+        let create_container_options = Some(CreateContainerOptions { 
+            name: &container_name,
+            platform: None,
+        });
 
-        // Get logs
+        let result = docker.create_container(create_container_options, config).await?;
+
+        Ok(result.id)
+    }
+
+    async fn stream_logs(
+        &self,
+        docker: &Docker,
+        container_id: &str,
+        job_name: &str
+    ) -> Result<(), bollard::errors::Error> {
+
         let mut logs = docker.logs(
-            &container,
+            &container_id,
             Some(LogsOptions::<String> {
                 follow: true,
                 stdout: true,
@@ -111,14 +156,29 @@ impl Job {
             }),
         );
 
-        debug!("Logs:");
+        let log_dir = format!("/tmp/oxipipe/artifacts/{}/logs", job_name);
+        fs::create_dir_all(&log_dir)?;
+
+        let log_file = format!("{}/output.log", log_dir);
+        let mut file = File::create(&log_file)?;
+
+        let mut wrote_output = false;
+
         while let Some(log_line) = logs.next().await {
             if let Ok(line) = log_line {
-                info!("{}", line);
+                debug!("{}", line);
+                write!(file, "{}", line.to_string())?;
+                file.flush()?;
+                wrote_output = true;
             }
         }
 
-        info!("Job {} completed.", name);
+        if !wrote_output {
+            writeln!(file, "No output")?;
+            file.flush()?;
+        }
+        
+        Ok(())
     }
 
 }
